8e3f03eff646ef0faa77cabbc5342fefbc2e1631
PartyLogan <l.nicol.dickson@gmail.com>
2023-07-12 21:53:55 +1000

    changed player color

--- Roguelike1834/src/Palette.h
+++ Roguelike1834/src/Palette.h
@@ -9,4 +9,4 @@ static const Color P_WHITE = { 255, 255, 255, 255 };
 static const Color P_DIRT = { 30, 9, 13, 255 };
 static const Color P_GRASS = { 0, 95, 65, 255 };
 static const Color P_TREES = { 8, 178, 59, 255 };
-static const Color P_PLAYER = { 236, 171, 17, 255 };
+static const Color P_PLAYER = { 236, 233, 16, 255 };
