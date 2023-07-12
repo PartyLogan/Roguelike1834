3b4729f82f6583df9a633215c4f332e881a0c5a4
PartyLogan <l.nicol.dickson@gmail.com>
2023-07-12 21:50:36 +1000

    palette change

--- Roguelike1834/src/Palette.h
+++ Roguelike1834/src/Palette.h
@@ -1,11 +1,12 @@
 #pragma once
 #include <raylib.h>
 
-static const Color P_BLACK = { 16, 18, 28, 255 };
+static const Color P_BLACK = { 0, 0, 0, 255 };
 static const Color P_WHITE = { 255, 255, 255, 255 };
 
-static const Color P_DIRT = { 44, 30, 49, 255 };
-static const Color P_GRASS = { 38, 133, 76, 255 };
-static const Color P_TREES = { 90, 181, 82, 255 };
-static const Color P_PLAYER = { 243, 168, 51, 255 };
 
+
+static const Color P_DIRT = { 30, 9, 13, 255 };
+static const Color P_GRASS = { 0, 95, 65, 255 };
+static const Color P_TREES = { 8, 178, 59, 255 };
+static const Color P_PLAYER = { 236, 171, 17, 255 };
