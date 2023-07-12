ef7e0983744cb63ca444bfeeeb859999de0c83eb
PartyLogan <l.nicol.dickson@gmail.com>
2023-07-13 01:08:21 +1000

    pixel prefect scaling

--- Roguelike1834/src/Roguelike1834.cpp
+++ Roguelike1834/src/Roguelike1834.cpp
@@ -11,11 +11,23 @@
 
 int main()
 {
-    const int screenWidth = 1280;
-    const int screenHeight = 720;
+    const int screenWidth = 1920;
+    const int screenHeight = 1080;
     const int tileSize = 16;
+    const int gameScreenWidth = 1280;
+    const int gameScreenHeight = 720;
     
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     InitWindow(screenWidth, screenHeight, "Roguelike");
+    SetWindowMinSize(1280, 720);
+
+    RenderTexture2D target = LoadRenderTexture(gameScreenWidth, gameScreenHeight);
+    SetTextureFilter(target.texture, TEXTURE_FILTER_POINT);
+
+    Camera2D camera = { 0 };
+    camera.offset = Vector2{ gameScreenWidth / 2 - tileSize/2, gameScreenHeight / 2 - tileSize/2 };
+    camera.zoom = 1.0f;
+    camera.rotation = 0.0f;
 
     // Load texture and setup renderer
     auto texture = LoadTexture("assets/monochrome-transparent_packed.png");
@@ -26,6 +38,7 @@ int main()
 
     // Create map
     Vector2 playerStart = gamestate.get()->map.get()->GenerateMap();
+    camera.target = playerStart;
 
     // Make player
     std::shared_ptr<Actor> player = std::make_shared<Actor>(playerStart.x, playerStart.y, P_PLAYER, P_BLACK, "Player", Tile::ActorTile::Player);
@@ -37,14 +50,28 @@ int main()
 
     while (!WindowShouldClose())
     {
-        BeginDrawing();
-        ClearBackground(BLACK);
+        float scale = std::round(std::min((float)GetScreenWidth() / gameScreenWidth, (float)GetScreenHeight() / gameScreenHeight));
 
         gamestate.get()->Update();
-        gamestate.get()->DrawMap();
-        gamestate.get()->DrawActors();
+        camera.target = Vector2{ (float)gamestate.get()->player.get()->x * tileSize, (float)gamestate.get()->player.get()->y * tileSize };
+        
+        BeginTextureMode(target);
+            BeginMode2D(camera);
+                ClearBackground(BLACK);
+                gamestate.get()->DrawMap();
+                gamestate.get()->DrawActors();
+            EndMode2D();
+        EndTextureMode();
 
-        DrawFPS(20, 20);
+        BeginDrawing();
+            ClearBackground(BLACK);
+
+            DrawTexturePro(target.texture, Rectangle{0.0f, 0.0f, (float)target.texture.width, (float)-target.texture.height },
+                Rectangle{(GetScreenWidth() - ((float)gameScreenWidth * scale)) * 0.5f, (GetScreenHeight() - ((float)gameScreenHeight * scale)) * 0.5f,
+                    (float)gameScreenWidth* scale, (float)gameScreenHeight* scale}, Vector2{ 0.0f, 0.0f }, 0.0f, WHITE);
+           
+            DrawFPS(20, 20);
+
         EndDrawing();
     }
     
